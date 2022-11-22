d4122cb3d8b86eba32e5e997fd8d820f9cdc1dd5
Asus <pateljenies5@gmail.com>
2022-11-22 12:18:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical9_20012011099/SMSViewAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.mad_practical9_20012011099
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.mad_practical9_20012011099.databinding.SmsItemViewBinding
+
+class SMSViewAdapter(context: Context, private val array:ArrayList<SMSView>):
+    ArrayAdapter<SMSView>(context,array.size,array) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val currentSms: SMSView? = getItem(position)
+        val binding = SmsItemViewBinding.inflate(LayoutInflater.from(context))
+        binding.textViewPhoneNo.text = currentSms!!.phoneno
+        binding.textViewMessage.text = currentSms!!.message
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/mad_practical9_20012011099/SMSview.kt
@@ -0,0 +1,5 @@
+package com.example.mad_practical9_20012011099
+
+class SMSView(val phoneno:String, val message:String) {
+
+}
